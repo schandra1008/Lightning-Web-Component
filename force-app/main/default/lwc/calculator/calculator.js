4f57b2e6554a9d4058508e19064f13049535d459
itsamar1 <amrendrachandra363@gmail.com>
2024-03-11 09:44:36 +0530

    commit 1.2

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Calculator extends LightningElement {
     firstNumber = 0;
@@ -13,4 +13,46 @@ export default class Calculator extends LightningElement {
     handleClick(event){
         
     }
+
+    /* change */ 
+    @track displayValue = '';
+    
+    buttons = [
+        { label: '7' },
+        { label: '8' },
+        { label: '9' },
+        { label: 'C' },
+        { label: '4' },
+        { label: '5' },
+        { label: '6' },
+        { label: '/' },
+        { label: '1' },
+        { label: '2' },
+        { label: '3' },
+        { label: '*' },
+        { label: '0' },
+        { label: '+' },
+        { label: '-' },
+        { label: '=' }
+    ];
+
+    handleButtonClick(event) {
+        const buttonValue = event.target.label;
+
+        if (buttonValue === 'C') {
+            // Clear the display
+            this.displayValue = '';
+        } else if (buttonValue === '=') {
+            // Evaluate the expression
+            try {
+                this.displayValue = eval(this.displayValue).toString();
+            } catch (error) {
+                this.displayValue = 'Error';
+            }
+        } else {
+            // Append the button value to the display
+            this.displayValue += buttonValue;
+        }
+    }
+
 }
